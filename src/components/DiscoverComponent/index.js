8fead1e71f55e364f14010d965b52f06d38274d5
Divyesh Parmar <divyesh.parmar@swoo.tv>
2019-04-16 18:39:45 +0530

    Carousal integration

--- src/components/DiscoverComponent/index.js
+++ src/components/DiscoverComponent/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './styles.css';
+import Slider from 'react-slick';
 
 import {ReactComponent as Gamepad} from '../../assets/group-8-copy.svg';
 
@@ -13,6 +14,12 @@ export default class DiscoverComponent extends Component{
 	}
 
 	render(){
+		var settings={
+			dots: true,
+			arrows: true,
+			infinite: true,
+			speed: 500,
+		}
 		return(
 			<div className="discover">
 				<div className="gamepad">
@@ -21,7 +28,22 @@ export default class DiscoverComponent extends Component{
 
 				<p className="newgame">Discover <br/>New games</p>
 				<p className="friends">Play popular games with friends & Use your skills to Win Big!</p>
-				<div>Carousel 1 Carousel 2</div>
+				<div className="carousel1">
+
+				</div>
+				<div className="carousel2">
+					<Slider {...settings}>
+						<div>
+							<h1>Divyesh</h1>
+						</div>
+						<div>
+							<h1>Parmar</h1>
+						</div>
+						<div>
+							<h1>Swoo</h1>
+						</div>
+					</Slider>
+				</div>
 			</div>
 		)
 	}
